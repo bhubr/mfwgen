b13d097a1d4bd93b54606e9882f9c85cc66d37ba
Benoît Hubert <benoithubert@gmail.com>
2020-04-08 01:21:19 +0200

    Generate tags.css from palette.yml

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'mustache', '~> 1.0'
+gem 'redcarpet', '~> 3.5'
--- mfwgen.rb
+++ mfwgen.rb
@@ -1,14 +1,49 @@
-# read _header.html, _body.html, _footer.html, _sidebar.html
-# inject content
+require 'yaml'
 require 'mustache'
 require 'redcarpet'
 
-# module ViewHelpers
-#   def tags
-#     p self
-#     self[:front_matter]
-#   end
-# end
+# https://stackoverflow.com/q/596216/#answer-56678483
+def contrast_color(r, g, b)
+  lum = (0.299 * r + 0.587 * g + 0.114 * b) / 255
+  puts "#{r}, #{g}, #{b} >> #{lum}"
+  if lum > 0.5
+    return "#000"
+  end
+  "#fff"
+end
+
+def color_to_rgb(hex)
+  r = hex[1..2].hex
+  g = hex[3..4].hex
+  b = hex[5..6].hex
+  # contrast_color(r, g, b)
+  [r, g, b]
+end
+
+def rgb_to_decimal(r, g, b)
+  [r / 255.0, g / 255.0, b / 255.0]
+end
+
+def s_rgb_to_lin(color_channel)
+  if color_channel <= 0.04045
+    return color_channel / 12.92
+  end
+  ((color_channel + 0.055) / 1.055) ** 2.4
+end
+
+def y_to_l_star(y)
+  return y < 216.0 / 24389 ? y ** (24389.0 / 27) : (y ** (1.0 / 3)) * 116 - 16
+end
+
+def get_fg_color(bg_hex)
+  r, g, b = color_to_rgb(bg_hex)
+  vr, vg, vb = rgb_to_decimal(r, g, b)
+  # print "#{r}/#{vr}, #{g}/#{vg}, #{b}/#{vb}"
+  luminance = 0.2126 * s_rgb_to_lin(vr) + 0.7152 * s_rgb_to_lin(vg) + 0.0722 * s_rgb_to_lin(vb)
+  l_star = y_to_l_star(luminance)
+  # puts "#{bg_hex} #{l_star}"
+  l_star >= 50 ? '#000' : '#fff'
+end
 
 class MfwGen < Mustache
   # include ViewHelpers
@@ -20,6 +55,27 @@ class MfwGen < Mustache
       Redcarpet::Render::HTML, extensions = {}
     )
     @template = File.read('template.mustache.html')
+    @palette = read_palette
+  end
+
+  def read_palette
+    palette_yaml = File.read('palette.yml')
+    palette_in = YAML.load(palette_yaml)
+    palette = {}
+    css = ""
+    palette_in.each do |key, bg_color|
+      fg_color = get_fg_color(bg_color)
+      palette[key] = {
+        :fg => fg_color,
+        :bg => bg_color
+      }
+
+      css += ".tag-#{key} { color: #{fg_color}; background: #{bg_color}; }\n"
+    end
+    open('tags.css', 'w') do |f|
+      f << css
+    end
+    palette
   end
 
   def to_html(md)
@@ -53,6 +109,11 @@ class MfwGen < Mustache
         :title => 'Rails',
         :content => to_html("Let's try some `inline code`."),
         :front_matter => 'title: Rails\ndescription: A random post on Ruby-on-Rails\ntags: ruby,ruby-on-rails\ndate: 2020-04-07'
+      },
+      {
+        :title => 'Monkey in Go&Ruby',
+        :content => to_html("Let's try some `inline code`."),
+        :front_matter => 'title: Rails&Go\ndescription: Anothe random post on Ruby-on-Rails\ntags: go,ruby,ruby-on-rails,js\ndate: 2020-04-07'
       }
     ]
     posts.each do |post|
