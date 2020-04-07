511ff82e428fea7eb5e206ee3f17749f547e9c48
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-04-07 09:33:42 +0200

    Ditch ERB and replace it with the almighty Mustache

--- mfwgen.rb
+++ mfwgen.rb
@@ -1,17 +1,25 @@
 # read _header.html, _body.html, _footer.html, _sidebar.html
 # inject content
-require 'erb'
+require 'mustache'
 require 'redcarpet'
 
-class MfwGen
-  #include ERB::util
+# module ViewHelpers
+#   def tags
+#     p self
+#     self[:front_matter]
+#   end
+# end
+
+class MfwGen < Mustache
+  # include ViewHelpers
   attr_accessor :template, :title
 
+  self.template_path = __dir__
   def initialize()
     @markdown = Redcarpet::Markdown.new(
       Redcarpet::Render::HTML, extensions = {}
     )
-    @template = File.read('body.erb.html')
+    @template = File.read('template.mustache.html')
   end
 
   def to_html(md)
@@ -19,16 +27,18 @@ class MfwGen
   end
 
   def render()
-    ERB.new(@template).result_with_hash({
+    Mustache.render(@template, {
       :title => 'Hello World!',
       :posts => [
         {
           :title => 'Ruby',
-          :content => to_html("I'm _really getting used_ to **Ruby**!")
+          :content => to_html("I'm _really getting used_ to **Ruby**!"),
+          :front_matter => 'title: Ruby\ndescription: A random post on Ruby\ntags: ruby\ndate: 2020-04-06'
         },
         {
           :title => 'Rails',
-          :content => to_html("Let's try some `inline code`.")
+          :content => to_html("Let's try some `inline code`."),
+          :front_matter => 'title: Rails\ndescription: A random post on Ruby-on-Rails\ntags: ruby,ruby-on-rails\ndate: 2020-04-07'
         }
       ]
     })
